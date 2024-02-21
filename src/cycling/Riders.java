a93b24abf255169c0225fc757941504e06bdc3a6
MattMcCree01 <matthewamccree@gmail.com>
2024-02-21 23:21:24 +0000

    start of creating Rider class Im not sure about how thwe methods are going to fit into the Cycling Portal

--- src/cycling/Riders.java
+++ src/cycling/Riders.java
@@ -1,5 +1,41 @@
 package cycling;
 
+import java.time.LocalTime;
+
 public class Riders {
-    
+    private int id;
+    private String name;
+    private int BirthYear;
+    private int RacePoints;
+    private int MountainPoints;
+    private int teamID;
+    private int[] stageResults;
+    private LocalTime elapsedTime;
+
+    public Riders (int teamId, String name, int yearOfBirth) {
+        this.teamID = teamId;
+        this.name = name;
+        this.BirthYear = yearOfBirth;
+        this.RacePoints = 0;
+        this.MountainPoints = 0;
+        this.stageResults = new int[0];
+        this.elapsedTime = LocalTime.of(0, 0, 0);
+    }
+    public void removeRider(int riderId) {
+        
+    }
+    public void registerRiderResultsInStage(int stageId, int riderId, LocalTime... checkpoints) {
+        
+    }
+    public LocalTime[] getRiderResultsInStage(int stageId, int riderId) {
+        
+    }
+    public LocalTime getRiderAdjustedElapsedTimeInStage(int stageId, int riderId) {
+        
+    }
+    public void deleteRiderResultsInStage(int stageId, int riderId) {
+        
+    }
+
+
 }
