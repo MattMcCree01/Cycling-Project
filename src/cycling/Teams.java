3caad7b593cb671c3c963de5ce56b29b7c13be10
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2024-02-22 18:30:22 +0000

    Creating Team class and methods

--- src/cycling/Teams.java
+++ src/cycling/Teams.java
@@ -1,5 +1,27 @@
 package cycling;
 
 public class Teams {
-    
+    private int teamID;
+    private String teamName;
+
+    public Teams(int teamID, String teamName) {
+        this.teamID = teamID;
+        this.teamName = teamName;
+    }
+
+    public void createTeam(String teamName) {
+
+    }
+
+    public void removeTeam(int teamID) {
+
+    }
+
+    public void getTeam() {
+
+    }
+
+    public void teamRiders() {
+
+    }
 }
