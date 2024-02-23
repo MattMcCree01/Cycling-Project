165496b4286d72019ccfd69bb6b1db8c3414c009
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2024-02-23 13:28:58 +0000

    Added description to the rider class

--- src/cycling/Teams.java
+++ src/cycling/Teams.java
@@ -3,11 +3,13 @@ package cycling;
 public class Teams {
     private int teamID;
     private String teamName;
+    private String description;
     private static int nextID = 1;
 
-    public Teams(String teamName) {
+    public Teams(String teamName, String description) {
         this.teamID = nextID++;
         this.teamName = teamName;
+        this.description = description;
     }
 
     public static void removeTeam(int teamID) {
