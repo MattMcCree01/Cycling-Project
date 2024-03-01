eef3057e95aea07de45796c4cd6b90fbdb6a842a
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2024-03-01 16:41:02 +0000

    Added the basic methods to the Team class. I have created a list of teams but im not sure if this is the correct thing to do so please change it if its wrong.

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -1,10 +1,12 @@
 package cycling;
+import java.util.List;
 
 public class Team {
     private int teamID;
     private String teamName;
     private String description;
     private static int nextID = 1;
+    private static List<Team> teams;    // Is this correct?
 
     public Team(String teamName, String description) {
         this.teamID = nextID++;
@@ -12,12 +14,24 @@ public class Team {
         this.description = description;
     }
 
-    public static void removeTeam(int teamID) {
-
+    public static void removeTeam(int teamID) throws IDNotRecognisedException {
+        for (int i = 0; i < teams.size(); i++) {
+            if (teams.get(i).teamID == teamID) {
+                teams.remove(i);
+                break;
+            } else
+                throw new IDNotRecognisedException("Team ID not recognised");
+        }
     }
 
-    public int getTeam() {
-        return teamID;
+    public int[] getTeams() {
+        int[] teamIDs = new int[teams.size()];
+
+        for (int i = 0; i < teams.size(); i++) {
+            teamIDs[i] = teams.get(i).teamID;
+        }
+
+        return teamIDs;
     }
 
     public void teamRiders() {
