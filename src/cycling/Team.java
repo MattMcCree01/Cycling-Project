052214eed284a7ea3ed15716a116abf92124c518
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2024-03-02 17:06:16 +0000

    Updated some code in Team.java

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -1,4 +1,5 @@
 package cycling;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
@@ -6,31 +7,34 @@ public class Team {
     private String teamName;
     private String description;
     private static int nextID = 1;
-    private static List<Team> teams;    // Is this correct?
+    private static List<Team> teams = new ArrayList<>();    // Is this correct?
 
     public Team(String teamName, String description) {
         this.teamID = nextID++;
         this.teamName = teamName;
         this.description = description;
+        teams.add(this);
     }
 
     public static void removeTeam(int teamID) throws IDNotRecognisedException {
+        boolean found = false;
         for (int i = 0; i < teams.size(); i++) {
             if (teams.get(i).teamID == teamID) {
                 teams.remove(i);
+                found = true;
                 break;
-            } else
-                throw new IDNotRecognisedException("Team ID not recognised");
+            }
+        }
+        if (!found) {
+            throw new IDNotRecognisedException("Team ID not recognised");
         }
     }
 
     public int[] getTeams() {
         int[] teamIDs = new int[teams.size()];
-
         for (int i = 0; i < teams.size(); i++) {
             teamIDs[i] = teams.get(i).teamID;
         }
-
         return teamIDs;
     }
 
