b888d95316f6d414e0e0ea28945144d1ddc6fa3f
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2024-03-05 23:01:42 +0000

    Completed the methods for stages

--- src/cycling/Stage.java
+++ src/cycling/Stage.java
@@ -3,7 +3,7 @@ package cycling;
 import java.time.LocalTime;
 
 public class Stage {
-    private int stageID;
+    private int stageId = 0;
     private int raceID;
     private String stageName;
     private StageType stageType;
@@ -12,40 +12,75 @@ public class Stage {
     private LocalTime startTime;
     private String status;
     private Checkpoint[] checkpoints; 
-    private static int nextID = 0;
 
     public Stage(int raceID, String stageName, StageType stageType, String stageDescription, double stageLength, LocalTime startTime) {
-        this.stageID = nextID++;
+        this.stageId = stageId++;
         this.raceID = raceID;
         this.stageName = stageName;
         this.stageType = stageType;
         this.stageDescription = stageDescription;
         this.stageLength = stageLength;
+        this.status = "preparation";
         this.startTime = startTime;
         this.checkpoints = new Checkpoint[0];
     }
     
     public int getStageId() {
-        return stageID;
+        return stageId;
     }
     
     public static void removeStage(int stageID) {
 
     }
 
-    public void addClimbToStage(int stageID, int checkpointID) {
-
+    public void addClimbToStage(int stageId, double location, double averageGradient, double length) {
+        Checkpoint newCheckpoint = new Checkpoint(stageId, location, averageGradient, length);
+        Checkpoint[] newCheckpoints = new Checkpoint[checkpoints.length + 1];
+        for (int i = 0; i < checkpoints.length; i++) {
+            newCheckpoints[i] = checkpoints[i];
+        }
+        newCheckpoints[checkpoints.length] = newCheckpoint;
+        checkpoints = newCheckpoints;
     }
 
-    public void addSprintToStage(int stageID, int checkpointID) {
-
+    public void addSprintToStage(int stageId, double location) {
+        Checkpoint newCheckpoint = new Checkpoint(stageId, location);
+        Checkpoint[] newCheckpoints = new Checkpoint[checkpoints.length + 1];
+        for (int i = 0; i < checkpoints.length; i++) {
+            newCheckpoints[i] = checkpoints[i];
+        }
+        newCheckpoints[checkpoints.length] = newCheckpoint;
+        checkpoints = newCheckpoints;
     }
 
-    public void removeCheckpointFromStage(int stageID, int checkpointID) {
+    public void removeCheckpointFromStage(int checkpointId) throws IDNotRecognisedException, InvalidStageStateException {
+        if (status.equals("waiting for results")) {
+            throw new InvalidStageStateException("Stage is in an invalid state for this operation");
+        }
 
+        int indexToRemove = -1;
+        for (int i = 0; i < checkpoints.length; i++) {
+            if (checkpoints[i].getCheckpointID() == checkpointId) {
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (indexToRemove != -1) {
+            checkpoints[indexToRemove] = null; // Set the reference to null
+            Checkpoint[] newCheckpoints = new Checkpoint[checkpoints.length - 1];
+            for (int i = 0, j = 0; i < checkpoints.length; i++) {
+                if (i != indexToRemove) {
+                    newCheckpoints[j++] = checkpoints[i];
+                }
+            }
+            checkpoints = newCheckpoints;
+        } else {
+            throw new IDNotRecognisedException("Checkpoint ID not recognised");
+        }
     }
 
-    public int[] getStageCheckpoints(int stageID) {
+    public int[] getStageCheckpoints(int stageId) {
         int[] outArray = new int[checkpoints.length];
         for (int i = 0; i < checkpoints.length; i++) {
             outArray[i] = checkpoints[i].getCheckpointID();
@@ -54,7 +89,7 @@ public class Stage {
         return outArray;
     }
 
-    public void concludeStagePreparation(int stageID) {
-
+    public void concludeStagePreparation(int stageId) {
+        this.status = "waiting for results";
     }
 }
